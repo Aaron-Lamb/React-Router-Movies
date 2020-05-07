004f514aa73df7d7e361b15086c4699026fecd40
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-06 17:55:10 -0700

    Added params.id as a dependency array int he useEffect of movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -21,7 +21,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[movie]);
+  },[movie, params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   const saveMovie = () => {
